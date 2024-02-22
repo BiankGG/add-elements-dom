73c2fc035604232543c62ba66ddbdba8a81e3c1c
Bianca G. Griffin <Biancagriffin@hotmail.es>
2024-02-22 16:14:56 +0100

    index.html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
  // Aquí tu código
+
+ 
+
+
+
+
+ const boton = document.getElementById('agregar')
+ const lista = document.getElementById('lista')
+ const buttonreset = document.getElementById('reset')
+ 
+ 
+ 
+ boton.addEventListener('click', function(){
+    const producto = prompt('Agregar Elementos');
+    if(producto){
+        let nuevoItem = document.createElement('li');
+        nuevoItem.textContent = producto;
+        lista.appendChild(nuevoItem);
+    }
+
+ })
+
+ buttonreset.addEventListener('click', function(){
+  lista.innerHTML = '';
+ });
+
+
+
+
+
+
+
+
